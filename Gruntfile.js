631bb0ac256fdc63ecb7b625ad8250dd64211b9d
Nick Schonning <nschonni@gmail.com>
2013-03-10 17:22:16 -0400

    Add asseble task
    Also fixed indenting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,59 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'qunit']
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		// Task configuration.
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				unused: true,
+				boss: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					jQuery: true
+				}
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib_test: {
+				src: ['lib/**/*.js', 'test/**/*.js']
+			}
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			lib_test: {
+				files: '<%= jshint.lib_test.src %>',
+				tasks: ['jshint:lib_test', 'qunit']
+			}
+		},
+		assemble: {
+			options: {
+				// Task-specific options go here.
+			},
+			theme_gcwu: {
+				// Target-specific file lists and/or options go here.
+			}
+		}
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('assemble');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit']);
-
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'assemble']);
 };
